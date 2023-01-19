213fc88dfffcdf17a1b7129e68404fd7deef0a3c
bestkiki <bestkiki3@gmail.com>
2023-01-20 00:05:22 +0900

    MakeShop constants

---
+++ constants.ts
@@ -0,0 +1,42 @@
+export const AVAILABLE_TAGS = [
+  {
+    display: '눈썹',
+    value: 'eyelash',
+  },
+  {
+    display: '입술',
+    value: 'lips',
+  },
+  {
+    display: '두피',
+    value: 'hair',
+  },
+  {
+    display: '튼살',
+    value: 'skin',
+  },
+  {
+    display: '미인점',
+    value: 'beautyMark',
+  },
+  {
+    display: '아이라인',
+    value: 'eyeline',
+  },
+  {
+    display: '속눈썹 펌',
+    value: 'innerEyeline',
+  },
+  {
+    display: '헤어라인',
+    value: 'hairLine',
+  },
+  {
+    display: '네일',
+    value: 'nail',
+  },
+  {
+    display: '패디',
+    value: 'pedicure',
+  },
+];
