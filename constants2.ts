e90970c637ddd67c0396e77d107e5d17f82c10be
bestkiki <bestkiki3@gmail.com>
2023-01-25 19:25:14 +0900

    Add files via upload

---
+++ constants2.ts
@@ -0,0 +1,31 @@
+export const AVAILABLE_TAGS2 = [
+  {
+    display: '월',
+    value: 'mon',
+  },
+  {
+    display: '화',
+    value: 'tue',
+  },
+  {
+    display: '수',
+    value: 'wed',
+  },
+  {
+    display: '목',
+    value: 'thu',
+  },
+  {
+    display: '금',
+    value: 'fri',
+  },
+  {
+    display: '토',
+    value: 'sat',
+  },
+  {
+    display: '일',
+    value: 'sun',
+  },
+  
+];
